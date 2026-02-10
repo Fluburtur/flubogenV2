628b23d77e9fecea0e94a3ae6d7b31501353c2b9
achan1989 <achan1989@gmail.com>
2026-02-10 21:39:09 +0000

    Disable all the debug printing

--- remote/remote_nano_v1/config.h
+++ remote/remote_nano_v1/config.h
@@ -5,7 +5,7 @@
 
 /* Uncomment the defines below to enable some debugging behaviours. */
 
-#define MAIN_DEBUGGING
+//#define MAIN_DEBUGGING
 //#define BUTT2MOD_DEBUGGING
-#define CONNECTED_FSM_DEBUGGING
-#define MESSAGES_DEBUGGING_HUMAN_READABLE
+//#define CONNECTED_FSM_DEBUGGING
+//#define MESSAGES_DEBUGGING_HUMAN_READABLE
